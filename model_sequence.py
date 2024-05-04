745da51c54864d56bd7067faac3ff60124954d89
newbie1223 <taiti7919@gmail.com>
2024-05-04 16:50:01 +0900

    add model_sequence

---
+++ model_sequence.py
@@ -0,0 +1,14 @@
+from transformers import AutoModelForSequenceClassification
+
+class_label = train_dataset.features['label']
+label2id = {label: id for id, label in enumerate(class_label.names)}
+id2label = {id: label for id, label in enumerate(class_label.names)}
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_name,
+    num_labels=class_label.num_classes,
+    label2id=label2id,
+    id2label=id2label
+)
+
+print(type(model).__name__)
+print(model.forward(**data_collator(endcoded_train_dataset[0:4])))
